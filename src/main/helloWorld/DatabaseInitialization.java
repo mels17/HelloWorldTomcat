550eeb72136494a285e549c2a16ae836e6b53047
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-19 10:37:23 +1000

    Modified travis.yml and added appspec.yml

--- src/main/helloWorld/DatabaseInitialization.java
+++ src/main/helloWorld/DatabaseInitialization.java
@@ -8,11 +8,14 @@ public class DatabaseInitialization {
     public static Connection init(String dbName, String dbUser, String dbUserPassword) throws ClassNotFoundException, SQLException {
         Connection connection = null;
 
-        System.out.println("Connecting to database...");
+
+        System.out.println("Loading driver...");
         Class.forName("org.postgresql.Driver");
+        System.out.println("Driver loaded!");
+        System.out.println("Connecting to database...");
+        // com.postgresql.jdbc.Driver
         connection = DriverManager
-                .getConnection("jdbc:postgresql://localhost:5432/" + dbName,
-                        dbUser, dbUserPassword);
+                .getConnection("jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password");
         System.out.println("Database connection successful.");
         return connection;
     }
