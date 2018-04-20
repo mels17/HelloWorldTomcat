b7267e2cf41108ac9205c5f881b8ce29cddc0a8a
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-20 10:47:05 +1000

    Changed class for postgresql

--- src/main/helloWorld/DatabaseInitialization.java
+++ src/main/helloWorld/DatabaseInitialization.java
@@ -8,10 +8,10 @@ public class DatabaseInitialization {
     public static Connection init(String dbName, String dbUser, String dbUserPassword) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         System.out.println("Loading driver...");
-        Class.forName("org.postgresql.Driver");
+        Class.forName("com.postgresql.jdbc.Driver");
         System.out.println("Driver loaded!");
         System.out.println("Connecting to database...");
-        // com.postgresql.jdbc.Driver
+        // org.postgresql.Driver
         // "jdbc:postgresql://localhost:5432/" + dbName, dbUser, dbUserPassword
         connection = DriverManager
                 .getConnection("jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password");
