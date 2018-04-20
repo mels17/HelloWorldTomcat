a5d9ef421c7524a0bf1d11c43b0d5ca08de6b8a0
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-20 10:04:11 +1000

    Changed to RDS instance

--- src/main/helloWorld/DatabaseInitialization.java
+++ src/main/helloWorld/DatabaseInitialization.java
@@ -7,13 +7,12 @@ import java.sql.SQLException;
 public class DatabaseInitialization {
     public static Connection init(String dbName, String dbUser, String dbUserPassword) throws ClassNotFoundException, SQLException {
         Connection connection = null;
-
-
         System.out.println("Loading driver...");
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded!");
         System.out.println("Connecting to database...");
         // com.postgresql.jdbc.Driver
+        // "jdbc:postgresql://localhost:5432/" + dbName, dbUser, dbUserPassword
         connection = DriverManager
                 .getConnection("jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password");
         System.out.println("Database connection successful.");
