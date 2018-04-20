7692f5c739f198eb2aef1b8564f844b766a5a9c3
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-20 10:59:46 +1000

    Added ord.postgresql.Driver

--- src/main/helloWorld/DatabaseInitialization.java
+++ src/main/helloWorld/DatabaseInitialization.java
@@ -8,10 +8,10 @@ public class DatabaseInitialization {
     public static Connection init(String dbName, String dbUser, String dbUserPassword) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         System.out.println("Loading driver...");
-        Class.forName("com.postgresql.jdbc.Driver");
+        Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded!");
         System.out.println("Connecting to database...");
-        // org.postgresql.Driver
+        // com.postgresql.jdbc.Driver
         // "jdbc:postgresql://localhost:5432/" + dbName, dbUser, dbUserPassword
         connection = DriverManager
                 .getConnection("jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password");
