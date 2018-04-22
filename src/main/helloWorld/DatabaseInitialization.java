5b54f27320625f29e0e5a12acdfbcdf4d54d13f1
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-23 09:19:15 +1000

    Modified to get system variables

--- src/main/helloWorld/DatabaseInitialization.java
+++ src/main/helloWorld/DatabaseInitialization.java
@@ -11,12 +11,18 @@ public class DatabaseInitialization {
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded!");
         System.out.println("Connecting to database...");
+        String dbHostname = System.getProperty("DB_HOSTNAME");
+        String dbPort = System.getProperty("DB_PORT");
+        String dbUsername = System.getProperty("DB_USERNAME");
+        String dbPassword = System.getProperty("DB_PASSWORD");
+        String name = System.getProperty("DB_NAME");
         // com.postgresql.jdbc.Driver
         // "jdbc:postgresql://localhost:5432/" + dbName, dbUser, dbUserPassword
         // "jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password"
+
+        String connectionString = "jdbc:postgresql://" + dbHostname + ":" +dbPort + "/" + name + "?user=" + dbUsername + "&password=" + dbPassword;
         connection = DriverManager
-                .getConnection("jdbc:postgresql://mel-helloworld-instance.czggd3g4trrj.ap-southeast-2.rds.amazonaws.com:5432/worldNames?user=malavika&password=password");
-        System.out.println("Database connection successful.");
+                .getConnection(connectionString);
         return connection;
     }
 
